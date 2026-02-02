fd88f7258d80c98e68d8418c8963a406f73944b6
narek.mkrtchyan <mkrtchyann1993@gmail.com>
2026-02-02 13:46:43 +0400

    update

--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -14,7 +14,7 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.WebHost.UseUrls("http://localhost:5001");
+        // builder.WebHost.UseUrls("http://localhost:5001");
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
